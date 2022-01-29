3f0083bdeaec55d8a9d6d284d964514fda05b0a9
Inchara  J A <aarushiii73@gmail.com>
2022-01-30 00:59:57 +0530

    updating pages

--- static/script.js
+++ static/script.js
@@ -1,26 +1,26 @@
- tailwind.config = {
-      theme: {
-        extend: {
-          colors: {
-            primary:"#0346f2"
-          },
-          transitionProperty: {
-        'left': 'left'
-      },
-          
-        }
-      }
-    }
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
 
-const mobiletoggle = document.getElementById("mobile-toggle");
-const drawer = document.getElementById("drawer");
-const drop = document.querySelector(".drop-down");
-const user = document.querySelector(".user");
-mobiletoggle.onclick = ()=>{
-  drawer.classList.toggle("left-0")
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
 }
 
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
 
-user.onclick = ()=>{
-  drop.classList.toggle("show")
+function next(){
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}
+
+function prev(){
+  slides[index].classList.remove('active');
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add('active');
 }
